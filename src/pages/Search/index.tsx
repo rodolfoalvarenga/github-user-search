2416d688fb1249dae2fd26144b2bdc039b17c61b
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-03 17:36:13 -0300

    GET data from GitHub

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,15 +1,51 @@
-import React from 'react';
+import { makeRequest } from 'core/utils/request';
+import React, { useState } from 'react';
 import BaseForm from './BaseForm';
 import './styles.scss';
 
+type FormState = {
+  login: string;
+};
+
+type FormEvent = React.ChangeEvent<HTMLInputElement>;
+
 const Search = () => {
+  const [formData, setFormData] = useState<FormState>({
+    login: '',
+  });
+
+  const handleOnChange = (event: FormEvent) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData((data) => ({ ...data, [name]: value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const payload = {
+      ...formData,
+    };
+
+    makeRequest({
+      url: `/${formData.login}`,
+      method: 'GET',
+      data: payload,
+    }).then(() => {
+      setFormData({ login: '' });
+    });
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <BaseForm title="Encontre um perfil Github">
         <div className="text-search">
           <input
+            value={formData.login}
+            name="login"
             type="text"
             className="form-control"
+            onChange={handleOnChange}
             placeholder="Usuário GitHub"
           />
         </div>
